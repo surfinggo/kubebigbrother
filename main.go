95ab001670e8a14b18c5ff73da255a4652f02894
wbsnail <wbsnail@pm.me>
2021-05-15 00:16:49 +0800

    Seed

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/spongeprojects/kubebigbrother/pkg/cmd"
+import (
+	"github.com/spongeprojects/kubebigbrother/pkg/cmd"
+	"math/rand"
+	"time"
+)
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	cmd.Execute()
 }
