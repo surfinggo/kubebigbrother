f7789e431337d342f7d9ca13b9d72c9c5ff5448e
wbsnail <wbsnail@pm.me>
2021-06-05 00:11:42 +0800

    fix: undefault env value

--- pkg/cmd/genericoptions/kubeconfig.go
+++ pkg/cmd/genericoptions/kubeconfig.go
@@ -3,9 +3,7 @@ package genericoptions
 import (
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
-	"github.com/spongeprojects/magicconch"
-	"k8s.io/client-go/util/homedir"
-	"path"
+	"os"
 )
 
 // KubeconfigOptions is kubeconfig options
@@ -22,6 +20,5 @@ func GetKubeconfigOptions() *KubeconfigOptions {
 
 // AddKubeconfigFlags adds kubeconfig flags to flag set
 func AddKubeconfigFlags(fs *pflag.FlagSet) {
-	defaultV := magicconch.Getenv("KUBECONFIG", path.Join(homedir.HomeDir(), ".kube", "config"))
-	fs.String("kubeconfig", defaultV, "path to kubeconfig file")
+	fs.String("kubeconfig", os.Getenv("KUBECONFIG"), "path to kubeconfig file")
 }
